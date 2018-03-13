cb4e9c0a373de7ce36e0daff2c324236766926eb
ruthmz <rmatosfer@gmail.com>
2018-03-13 04:07:23 -0500

    arreglando estructura

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 // La funcionalidad de tu proyecto
+$("#wrapper").click( function() {
+	$(".menu").toggleClass("close");
+});
+$(document).on('click', 'a[href^="#"]', function(event){
+  event.preventDefault();
+  $('html,body').animate({
+  scrollTop: $($.attr(this, 'href')).offset().top
+  }, 1850);
+});
