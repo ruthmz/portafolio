665ba8497b4607918f28452286a036e5db4f2ccc
ruthmz <rmatosfer@gmail.com>
2018-04-06 21:39:03 -0500

    arreglando menú hamburguesa

--- js/app.js
+++ js/app.js
@@ -1,10 +1,63 @@
 // La funcionalidad de tu proyecto
-$("#wrapper").click( function() {
-	$(".menu").toggleClass("close");
-});
+
 $(document).on('click', 'a[href^="#"]', function(event){
   event.preventDefault();
   $('html,body').animate({
   scrollTop: $($.attr(this, 'href')).offset().top
   }, 1850);
+});
+
+$(document).ready(function () {
+
+  //Open the menu
+  jQuery("#hamburger").click(function () {
+
+      jQuery('#content').css('min-height', jQuery(window).height());
+
+      jQuery('nav').css('opacity', 1);
+
+      //set the width of primary content container -> content should not scale while animating
+      var contentWidth = jQuery('#content').width();
+
+      //set the content with the width that it has originally
+      jQuery('#content').css('width', contentWidth);
+
+      //display a layer to disable clicking and scrolling on the content while menu is shown
+      jQuery('#contentLayer').css('display', 'block');
+
+      //disable all scrolling on mobile devices while menu is shown
+      jQuery('#container').bind('touchmove', function (e) {
+          e.preventDefault()
+      });
+
+      //set margin for the whole container with a jquery UI animation
+      //use stop(true) to fix jQuery overlapping issue with animation
+      //if the first argument is true, removes all animations in the queue
+      jQuery("#container").stop(true).animate({"marginLeft": ["70%", 'easeOutExpo']}, {
+          duration: 700
+      });
+
+  });
+
+  //close the menu
+  jQuery("#contentLayer").click(function () {
+
+      //enable all scrolling on mobile devices when menu is closed
+      jQuery('#container').unbind('touchmove');
+
+      //set margin for the whole container back to original state with a jquery UI animation
+      //use stop(true) to fix jQuery overlapping issue with animation
+      //if the first argument is true, removes all animations in the queue
+      jQuery("#container").stop(true).animate({"marginLeft": ["-1", 'easeOutExpo']}, {
+          duration: 700,
+          complete: function () {
+              jQuery('#content').css('width', 'auto');
+              jQuery('#contentLayer').css('display', 'none');
+              jQuery('nav').css('opacity', 0);
+              jQuery('#content').css('min-height', 'auto');
+
+          }
+      });
+  });
+
 });
